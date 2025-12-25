05ba473bd75bfd5a21ce9a8d3d34d2315e3b2efd
sukanyabeuria <sukanyabeuriasukanya@gmail.com>
2025-12-25 10:46:43 +0530

    GCD.java

---
+++ GCD.java
@@ -0,0 +1,13 @@
+class GCD {
+    public static void main(String[] args) {
+        int a = 36, b = 24;
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println(a);
+    }
+}
